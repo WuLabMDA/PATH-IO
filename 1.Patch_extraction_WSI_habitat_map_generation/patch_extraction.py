c9fdb833541120114a82a1c74c6d12222eded895
Rukhmini Roy <49904824+Rukhmini@users.noreply.github.com>
2026-05-18 12:39:47 -0500

    Update patch_extraction.py

--- 1.Patch_extraction_WSI_habitat_map_generation/patch_extraction.py
+++ 1.Patch_extraction_WSI_habitat_map_generation/patch_extraction.py
@@ -1,134 +1,139 @@
-import sys, os, glob
+import os
+import sys
+import glob
+import time
 import math
 import numpy as np
 from PIL import Image
-
 import openslide
 from multiprocessing import Pool
-import time
-import pdb
-from preprocess import apply_image_filters, tissue_percent, mask_percent
 
 
-def get_downsampled_image(slide, scale_factor=32):
-    large_w, large_h=slide.dimensions
-    new_w = math.floor(large_w/scale_factor)
-    new_h = math.floor(large_h/scale_factor)    
+def get_downsampled_image(slide, scale_factor=1):
+    large_w, large_h = slide.dimensions
+    new_w = math.floor(large_w / scale_factor)
+    new_h = math.floor(large_h / scale_factor)
+
     level = slide.get_best_level_for_downsample(scale_factor)
     whole_slide_image = slide.read_region((0, 0), level, slide.level_dimensions[level])
     whole_slide_image = whole_slide_image.convert("RGB")
     img = whole_slide_image.resize((new_w, new_h), Image.Resampling.BILINEAR)
+
     return img, new_w, new_h
 
-def get_start_end_coordinates(x, tile_size):
-    start = int(x * tile_size)
-    end = int((x+1) * tile_size)
-    return start, end
 
-def wsi_patch(start_h, end_h, start_w, end_w, tile_size, savepath, slide_list, num_tiles_w, num_tiles_h, idx, N, prop):
-    tile_region = tissue[start_h:end_h, start_w:end_w]
+def get_start_coordinate(x, tile_size):
+    return int(x * tile_size)
+
+
+def wsi_patch(start_h, start_w, tile_size, savepath, slide_name,
+              num_tiles_w, num_tiles_h, app_mag, slide_path):
+
+    slide = openslide.open_slide(slide_path)
+
     tile = slide.read_region((start_w, start_h), 0, (tile_size, tile_size))
     tile = tile.convert("RGB")
 
-    
-    tile_path = savepath + slide_list + '_' + str(tile_size) + '_x'+ str(start_w) + '_'+ str(int(start_w/tile_size))+ \
-    '_'+str(num_tiles_w) + '_y'+str(start_h)+'_'+str(int(start_h/tile_size))+'_'+str(num_tiles_h)+'.png'
-    
-    
-    if prop == '40':
+    tile_path = os.path.join(
+        savepath,
+        f"{slide_name}_{tile_size}"
+        f"_x{start_w}_{int(start_w / tile_size)}_{num_tiles_w}"
+        f"_y{start_h}_{int(start_h / tile_size)}_{num_tiles_h}.png"
+    )
+
+    if app_mag == "40":
         tile = tile.resize((256, 256), Image.Resampling.LANCZOS)
+
     tile.save(tile_path)
+    slide.close()
 
 
-if __name__ == '__main__':
-    
-    '''
-    example: python svs2tile.py 0 20
-    '''
-    
-    slide_rootpath = '/path/to/wsi_directory/'     # Input WSI directory
-    out_rootpath   = '/path/to/output_directory/'  # Output directory
+if __name__ == "__main__":
 
+    """
+    Example:
+    python svs2tile.py 0 20
+    """
 
-    
-    if not os.path.exists(out_rootpath):
-        os.makedirs(out_rootpath)  
-    
-    slide_list = [os.path.split(i)[1][:-4] for i in glob.glob(slide_rootpath+'*.svs')]
-    PATCH_FUN = 'CUSTOM'
+    slide_rootpath = "/path/to/wsi_directory/"      # Input WSI directory
+    out_rootpath = "/path/to/output_directory/"     # Output patch directory
 
-    start = sys.argv[1]
-    stop = sys.argv[2]
-    if int(stop) > len(slide_list):
-        stop = str(len(slide_list))
+    os.makedirs(out_rootpath, exist_ok=True)
+
+    slide_paths = sorted(glob.glob(os.path.join(slide_rootpath, "*.svs")))
+    slide_list = [os.path.splitext(os.path.basename(i))[0] for i in slide_paths]
+
+    start = int(sys.argv[1])
+    stop = int(sys.argv[2])
+
+    if stop > len(slide_list):
+        stop = len(slide_list)
 
     scale_factor = 1
 
-    for i in range (int(start), int(stop)):
-        savepath = out_rootpath + slide_list[i] + '/'
-        
+    for i in range(start, stop):
+
+        slide_name = slide_list[i]
+        slide_path = slide_paths[i]
+        savepath = os.path.join(out_rootpath, slide_name)
+
         if os.path.exists(savepath):
-            print('Slide %s is already processed. Skipping.' % slide_list[i])
+            print(f"Slide {slide_name} is already processed. Skipping.")
             continue
-            
-        slide_path=slide_rootpath + slide_list[i] + '.svs'
+
+        os.makedirs(savepath, exist_ok=True)
+
         slide = openslide.open_slide(slide_path)
         prop = slide.properties
         start_time = time.time()
-        
-        if not os.path.exists(savepath):
-            os.makedirs(savepath) 
 
-        if prop['aperio.AppMag']=='40':
-            print('AppMag is 40')
-            tile_size=512
-        elif prop['aperio.AppMag']=='20':
-            print('AppMag is 20')
-            tile_size=256
+        app_mag = prop.get("aperio.AppMag", None)
+
+        if app_mag == "40":
+            print("AppMag is 40")
+            tile_size = 512
+        elif app_mag == "20":
+            print("AppMag is 20")
+            tile_size = 256
         else:
-            print('AppMag is %s for %s' % (prop['aperio.AppMag'], slide_list[i]))
+            print(f"AppMag is {app_mag} for {slide_name}. Skipping.")
+            slide.close()
             continue
 
-        img, new_w, new_h = get_downsampled_image(slide, scale_factor=scale_factor)
-        tissue=(np.array(img))
+        _, new_w, new_h = get_downsampled_image(slide, scale_factor=scale_factor)
 
-        small_tile_size = int(tile_size)
-        num_tiles_h = new_h//small_tile_size
-        num_tiles_w = new_w//small_tile_size
+        num_tiles_h = new_h // tile_size
+        num_tiles_w = new_w // tile_size
 
-        # pdb.set_trace()
+        slide.close()
 
-        print('Start for %s' % slide_list[i])
+        print(f"Start for {slide_name}")
 
-        PATCH_FUN = 'CUSTOM'
+        items = []
 
-        if PATCH_FUN == 'CUSTOM':
-            #-----------------------------------customized function to cut whole slide into png and save as hd5------------------------------
-            idx = 0
-            start_h_vec = np.zeros(num_tiles_h*num_tiles_w, dtype = int)
-            end_h_vec = np.zeros(num_tiles_h*num_tiles_w, dtype = int)
-            start_w_vec = np.zeros(num_tiles_h*num_tiles_w, dtype = int)
-            end_w_vec = np.zeros(num_tiles_h * num_tiles_w, dtype = int)
-            for h in range(num_tiles_h):
-                for w in range(num_tiles_w):
-                    start_h, end_h = get_start_end_coordinates(h, tile_size)
-                    start_w, end_w = get_start_end_coordinates(w, tile_size)
-                
-                    start_h_vec[idx] = start_h
-                    end_h_vec[idx] = end_h
-                    start_w_vec[idx] = start_w
-                    end_w_vec[idx] = end_w
-                    idx = idx+1
+        for h in range(num_tiles_h):
+            for w in range(num_tiles_w):
+                start_h = get_start_coordinate(h, tile_size)
+                start_w = get_start_coordinate(w, tile_size)
 
-            print('total patches: %s' % idx)
+                items.append(
+                    (
+                        start_h,
+                        start_w,
+                        tile_size,
+                        savepath,
+                        slide_name,
+                        num_tiles_w,
+                        num_tiles_h,
+                        app_mag,
+                        slide_path,
+                    )
+                )
 
-            with Pool() as pool:
-                # prepare arguments
-                items = [((start_h_vec[kk], end_h_vec[kk], start_w_vec[kk], end_w_vec[kk], tile_size, savepath, slide_list[i], num_tiles_w, num_tiles_h, kk, idx, prop['aperio.AppMag'])) for kk in range(idx)]
-                # call the same function with different data in parallel
-                pool.starmap(wsi_patch, items)
-            
-            
-            print('Done for %s' % slide_list[i])
-            print("--- %s seconds ---" % (time.time() - start_time))
+        print(f"Total patches: {len(items)}")
 
+        with Pool() as pool:
+            pool.starmap(wsi_patch, items)
+
+        print(f"Done for {slide_name}")
+        print(f"--- {time.time() - start_time:.2f} seconds ---")
